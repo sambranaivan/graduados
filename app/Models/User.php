500b8487edd39d7c9d21269f9ea284c14c85c8e0
Carlos Alberto Romero <cromero2386@gmail.com>
2017-02-02 16:14:42 -0300

    Modificacion modelo usuario

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,8 +3,11 @@
 namespace graduados\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Bican\Roles\Traits\HasRoleAndPermission;
+use Bican\Roles\Contracts\HasRoleAndPermission as HasRoleAndPermissionContract;
 
-class User extends Model {
+class User extends Model implements AuthenticatableContract, CanResetPasswordContract, HasRoleAndPermissionContract {
+	use Authenticatable, CanResetPassword, HasRoleAndPermission;
 	protected $table = 'users';
 	protected $primaryKey = 'user_id';
 	protected $fillable = ['name', 'password', 'email','phone'];
