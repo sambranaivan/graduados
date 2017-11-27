dd7ae30b7198ea49fc1f3acc8d3bc0179f529973
Carlos Romero <cromero2386@gmail.com>
2017-11-27 17:46:14 -0300

    Agrega campo destacado en noticias, valor por defecto 0

--- app/Models/News.php
+++ app/Models/News.php
@@ -9,16 +9,16 @@ class News extends Model
 {
 	protected $table='news';
 	protected $primaryKey = 'new_id';
-	protected $fillable=['title','pompadour','body','photo','typenew_id','career_id','great', 'publication_date', 'end_publication'];
-    
+	protected $fillable=['title','pompadour','body','photo','typenew_id','career_id','great', 'publication_date', 'end_publication','destacado'];
+
     public $timestamps = true;
 
     public function type() {
 		return $this->belongsTo(TypeNew::class,'typenew_id');
 	}
-    
+
     public function career() {
 		return $this->belongsTo(Career::class,'career_id');
 	}
-	
+
 }
